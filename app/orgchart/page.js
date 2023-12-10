d5b11f6ee2eefb5af46f7220f12eb3322447b37e
Omar Abdiwali <omarabdiwali17@gmail.com>
2023-12-09 21:46:25 -0600

    idk

---
+++ app/orgchart/page.js
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from 'react'
+import CollapsibleTable from '@/component/CollapsibleTable';
+
+export default function Home() {
+  const [orgData, setOrgData] = useState([]);
+
+  useEffect(() => {
+    fetch('/org-chart').then(res => res.json())
+      .then(data => setOrgData(data)).catch(err => console.error(err));
+  }, [])
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className='mt-5 mb-10 mx-5 flex flex-row space-x-3'>
+        <a href="/find-employee" className='hover:underline'>Find Employee</a>
+        <div className='text-slate-400'>/</div>
+        <a href='/format-csv' className='hover:underline'>Format CSV String</a>
+      </div>
+      <CollapsibleTable rows={orgData} />
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,23 +1,12 @@
 "use client";
-import { useEffect, useState } from 'react'
-import CollapsibleTable from '@/component/CollapsibleTable';
-
-export default function Home() {
-  const [orgData, setOrgData] = useState([]);
-
-  useEffect(() => {
-    fetch('/orgchart').then(res => res.json())
-      .then(data => setOrgData(data)).catch(err => console.error(err));
-  }, [])
-
+export default function Home() {  
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className='mt-5 mb-10 mx-5 flex flex-row space-x-3'>
-        <a href="/find-employee" className='hover:underline'>Find Employee</a>
-        <div className='text-slate-400'>/</div>
-        <a href='/format-csv' className='hover:underline'>Format CSV String</a>
-      </div>
-      <CollapsibleTable rows={orgData} />
+    <div className="min-h-screen flex flex-row space-x-3">
+      <a href="/orgchart" className="hover:underline">Organization Chart</a>
+      <div className="text-slate-400">/</div>
+      <a href="/find-employee" className="hover:underline">Find Employee(s)</a>
+      <div className="text-slate-400">/</div>
+      <a href="/format-csv" className="hover:underline">Format CSV String</a>
     </div>
   )
 }
